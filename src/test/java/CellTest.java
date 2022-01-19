09f2bfc1d95e79abc9c8bacf8012e9ff6794b2a4
JonathanLissman <facehump90@gmail.com>
2022-01-19 12:54:42 +0100

    added new tests

--- src/test/java/CellTest.java
+++ src/test/java/CellTest.java
@@ -4,13 +4,33 @@ import static org.junit.Assert.*;
 
 public class CellTest {
 
+
+
     @Test
-    public void IfOneNeighborThisDies()  {
+    public void IfZeroNeighborsThisDies(){
         Cell cell = new Cell(Cell.CellState.ALIVE);
 
         Cell.CellState actual = cell.GetNextState(1);
 
         assertEquals(Cell.CellState.DEAD, actual);
+    }
+
+    @Test
+    public void IfOneNeighborsThisDies()  {
+        Cell cell = new Cell(Cell.CellState.ALIVE);
+
+        Cell.CellState actual = cell.GetNextState(1);
+
+        assertEquals(Cell.CellState.DEAD, actual);
+    }
+
+    @Test
+    public void IfTwoNeighborsThisLivesOn() {
+        Cell cell = new Cell(Cell.CellState.ALIVE);
+
+        Cell.CellState actual = cell.GetNextState(2);
+
+        assertEquals(Cell.CellState.ALIVE, actual);
     }
 
 }
