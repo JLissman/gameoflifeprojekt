708825b286e95e977159ed63b2d52ce1374a7b54
JonathanLissman <facehump90@gmail.com>
2022-01-21 11:58:13 +0100

    added constructor to simulation class

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,7 +1,20 @@
+
+
+
+
 public class Simulation {
 
-    public Simulation(Cell.CellState[][] cellStates){
+    private Cell[][] state;
 
+
+    public Simulation(Cell.CellState[][] cellStates){
+        state = new Cell[cellStates.length][];
+        for (int rad = 0; rad < cellStates.length; rad++) {
+            state[rad] = new Cell[cellStates[rad].length];
+            for (int kolumn = 0; kolumn < cellStates[rad].length; kolumn++) {
+                state[rad][kolumn] = new Cell(cellStates[rad][kolumn]);
+            }
+        }
     }
 
     public Cell.CellState[][] getState(){
